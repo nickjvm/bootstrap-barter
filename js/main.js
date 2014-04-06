cc19c824ce2fa615d347a4355f7b6c28027e8e13
Nick VanMeter <nickjvm@gmail.com>
2014-04-06 09:20:46 -0700

    Js commenting.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
+	
+	//taginputs
 	$("input.make-tags").tagsInput();
 
+	//tipsy
 	$(".tipsified").tipsy({
 		gravity:"w",
 		offset:5,
 	});
+
+	//fixed position sidebar on scroll
 	$(window).on("scroll",function() {
 		var $filters = $(".filters");
 		if($(this).scrollTop() > $(".sidebar").offset().top) {
